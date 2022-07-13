2b0ae69f9c562213e98a7bf86bc9048b0574a6f1
AleTartaglia <alej.tartaglia@gmail.com>
2022-07-12 21:07:43 -0300

    fix img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { useState, useEffect } from "react/cjs/react.development";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
+import helmet from "./assets/img/helmet-1.png";
+import potion from "./assets/img/potion-1.png";
+import ring from "./assets/img/ring-1.png";
+import scroll from "./assets/img/scroll-1.png";
+import shield from "./assets/img/shield-1.png";
+import sword from "./assets/img/sword-1.png";
 
 const cardImages = [
-  { src: "/img/helmet-1.png", matched: false },
-  { src: "/img/potion-1.png", matched: false },
-  { src: "/img/ring-1.png", matched: false },
-  { src: "/img/scroll-1.png", matched: false },
-  { src: "/img/shield-1.png", matched: false },
-  { src: "/img/sword-1.png", matched: false },
+  { src: helmet, matched: false },
+  { src: potion, matched: false },
+  { src: ring, matched: false },
+  { src: scroll, matched: false },
+  { src: shield, matched: false },
+  { src: sword, matched: false },
 ];
 
 function App() {
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SingleCard.css";
+import cover from "../assets/img/cover.png";
 
 const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
   const handleClick = () => {
@@ -14,7 +15,7 @@ const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
         <img className="front" src={card.src} alt="card front" />
         <img
           className="back"
-          src="/img/cover.png"
+          src={cover}
           onClick={handleClick}
           alt="card back"
         />
